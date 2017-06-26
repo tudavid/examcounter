2487bd62f0730a183832e4d0934dca2e9d7ea359
David Alt <davidalt@web.de>
2017-06-26 13:30:08 +0200

    fixed bug hours not displayed correctly

--- examcounter.py
+++ examcounter.py
@@ -12,7 +12,6 @@ for line in exams:
 			examdate = datetime.strptime(i, '%Y-%m-%d %H:%M:%S ' )
 			currentdate = datetime.now()
 			timebetween = examdate - currentdate
-			
 			pair1 = timebetween
 			pair = pair2, pair1
 			listofexamdates.append(pair)
@@ -23,8 +22,8 @@ listofexamdates.sort(key=lambda x: x[1])
 output=" "
 for j in listofexamdates:
 	a,b = j
-	output +=a + ":"
-	output += " [" + str(b).split(" ")[0] + "d"
-	output += str(b).split(":")[1] + "h"
+	output +=a + ":" + "["
+	output += str(b.days) + "d"
+	output += str(b.seconds//3600) + "h"
 	output += "] "
 print output
