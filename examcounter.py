933adad15973e42048a6202a9e724243e6fcfdd1
David Alt <davidalt@web.de>
2017-08-14 15:24:57 +0200

    now negative deltas are not displayed anymore

--- examcounter.py
+++ examcounter.py
@@ -3,21 +3,25 @@ import os
 
 exams = open((os.path.dirname(__file__) + "/examdates"), "r")
 listofexamdates = []
-for line in exams:
-	parts = line.split(";")
-	counter = 0
-	for i in parts:
-		#print i
-		if(counter % 2 == 1):
-			examdate = datetime.strptime(i, '%Y-%m-%d %H:%M:%S ' )
-			currentdate = datetime.now()
-			timebetween = examdate - currentdate
-			pair1 = timebetween
-			pair = pair2, pair1
-			listofexamdates.append(pair)
-		else:
-			pair2 = i
-		counter+=1
+datecounter = 0
+while (datecounter < 6):
+    for line in exams:
+	    parts = line.split(";")
+	    counter = 0
+	    for i in parts:
+		    #print i
+		    if(counter % 2 == 1):
+			    examdate = datetime.strptime(i, '%Y-%m-%d %H:%M:%S ')
+			    currentdate = datetime.now()
+			    timebetween = examdate - currentdate
+			    if(timebetween >=  timedelta(minutes=0)):
+			        pair1 = timebetween
+			        pair = pair2, pair1
+			        listofexamdates.append(pair)
+		    else:
+			    pair2 = i
+		    counter+=1
+    datecounter+=1
 listofexamdates.sort(key=lambda x: x[1])
 output=" "
 for j in listofexamdates:
